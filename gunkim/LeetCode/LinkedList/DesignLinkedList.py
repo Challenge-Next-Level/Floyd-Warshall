e119d429e05f9bf55a2b22b3b1c14ff972907578
unknown <harrison1995@naver.com>
2022-01-20 18:00:06 +0900

    아직도 수정중

--- gunkim/LeetCode/LinkedList/DesignLinkedList.py
+++ gunkim/LeetCode/LinkedList/DesignLinkedList.py
@@ -11,10 +11,16 @@ class MyLinkedList:
 
 
     def get(self, index: int) -> int:
-        i = 0
-        while i < index:
-            self = self.next
-            i += 1
+        if index < 0 or index >= self.size:
+            return -1
+
+        if self.head is None:
+            return -1
+
+        curr = self.head
+        for i in range(index):
+            curr = curr.next
+        return curr.val
 
     def addAtHead(self, val: int) -> None:
         node = Node(val)
@@ -23,26 +29,45 @@ class MyLinkedList:
         self.size += 1
 
     def addAtTail(self, val: int) -> None:
-        node = Node(val)
-        tail = self.head
-        for _ in range(self.size - 1):
-            tail = tail.next
-        tail.next = node
-        self.size += 1
-
         curr = self.head
-        if curr is None:
+        if curr is None: # self에 node가 하나도 없을때
             self.head = Node(val)
-        else:
-            while curr.next is not None:
+        else: # node가 있다면
+            while curr.next is not None: # 마지막 node를 찾는다
                 curr = curr.next
-            curr.next = Node(val)
+            curr.next = Node(val) # 마지막에 노드 연결
 
         self.size += 1
 
     def addAtIndex(self, index: int, val: int) -> None:
+        if index < 0 or index > self.size: # index가 LinkedList의 범위를 벗어날때
+            return
+
+        if index == 0: # 0번 째는 addAtHead() 활용
+            self.addAtHead(val)
+        else: # index번째를 찾아 노드 삽입
+            curr = self.head
+            for i in range(index - 1):
+                curr = curr.next
+            node = Node(val)
+            node.next = curr.next
+            curr.next = node
+
+            self.size += 1
 
     def deleteAtIndex(self, index: int) -> None:
+        if index < 0 or index > self.size: # index가 LinkedList의 범위를 벗어날때
+            return
+
+        curr = self.head
+        if index == 0:
+            self.head = curr.next
+        else:
+            for i in range(index - 1):
+                curr = curr.next
+            curr.next = curr.next.next
+
+        self.size -= 1
 
 # Your MyLinkedList object will be instantiated and called as such:
 # obj = MyLinkedList()
