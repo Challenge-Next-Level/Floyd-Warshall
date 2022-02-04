bc37273839c507a26683ece0c12ac1d521ccfb37
unknown <harrison1995@naver.com>
2022-02-04 11:22:15 +0900

    hard: 문자열 비교 dp

--- gunkim/Baekjoon/week03_DP/9251_LCS.py
+++ gunkim/Baekjoon/week03_DP/9251_LCS.py
@@ -3,13 +3,13 @@ read = sys.stdin.readline
 
 word1, word2 = read().strip(), read().strip()
 l1, l2 = len(word1), len(word2)
-cache = [0] * l2
+# matrix 표 생성, 이때 초기값을 따로 더 만들어야 해서 +1로 칸을 늘린다
+matrix = [[0] * (l2 + 1) for _ in range(l1 + 1)]
 
-for i in range(l1):
-    cnt = 0
-    for j in range(l2):
-        if cnt < cache[j]:
-            cnt = cache[j]
-        elif word1[i] == word2[j]:
-            cache[j] = cnt + 1
-print(max(cache))
+for i in range(1, l1 + 1): # 1번째 줄 부터 matrix표를 채워간다. (word1의 i번째 까지 문자열)
+    for j in range(1, l2 + 1): # word2의 j번째 까지 문자열과 비교
+        if word1[i - 1] == word2[j - 1]: # 비교하는 문자가 같다면 [i - 1][j - 1] + 1
+            matrix[i][j] = matrix[i - 1][j - 1] + 1
+        else: # 아니라면 max([i - 1][j], [i][j - 1])
+            matrix[i][j] = max(matrix[i - 1][j], matrix[i][j - 1])
+print(matrix[l1][l2])
