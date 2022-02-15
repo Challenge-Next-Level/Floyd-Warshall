5c0be62c25484efbb0c673f2f6ac8668d0805413
unknown <harrison1995@naver.com>
2022-02-15 16:31:56 +0900

    오랜만에 이분탐색 다행히 통과

---
+++ gunkim/Baekjoon/week06_BinarySearch/7795_먹을것인가먹힐것인가.py
@@ -0,0 +1,23 @@
+import sys
+
+T = int(sys.stdin.readline())
+for _ in range(T):
+    N, M = map(int, sys.stdin.readline().split())
+    A = list(map(int, sys.stdin.readline().split()))
+    B = list(map(int, sys.stdin.readline().split()))
+    B.sort() # 이분탐색을 위해 정렬 필수
+    answer = 0 # A가 B보다 큰 쌍의 갯수(총합)
+
+    for i in range(len(A)): # 모든 A에 대해 이분 탐색
+        left, right = 0, len(B) - 1
+        cnt = 0 # A가 B보다 큰 쌍의 갯수
+        while left <= right:
+            num = (left + right) // 2 # 중간 index를 설정
+            if A[i] <= B[num]:
+                right = num - 1
+            else:
+                left = num + 1
+                cnt = left
+        answer += cnt
+
+    print(answer)
