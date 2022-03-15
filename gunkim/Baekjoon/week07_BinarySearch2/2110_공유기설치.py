342593ddcc2e5d7e53c91c3246eafc9722bc4d94
unknown <harrison1995@naver.com>
2022-03-15 17:39:27 +0900

    이전에 쓴 코드를 오랜만에 와서 추가로 적었는데 풀리네

--- gunkim/Baekjoon/week07_BinarySearch2/2110_공유기설치.py
+++ gunkim/Baekjoon/week07_BinarySearch2/2110_공유기설치.py
@@ -1,12 +1,14 @@
 import sys
 
-N, C = map(int, sys.stdin.readline().split())
+N, C = map(int, sys.stdin.readline().split()) # 집, 공유기 갯수
 coordinate = []
-for _ in range(N):
+for _ in range(N): # 집의 좌표
     coordinate.append(int(sys.stdin.readline().split()[0]))
 coordinate.sort()
 
 
+# 임의로 distance(거리) 값을 정한 후 공유기를 설치한다.
+# 설치한 공유기 갯수를 반환하고 그 결과를 통해 이진 탐색 범위 결정에 활용.
 def install_wifi(distance):
     current = coordinate[0]
     cnt = 1
@@ -17,8 +19,14 @@ def install_wifi(distance):
     return cnt
 
 
-left, right = 0, N - 1
+answer = 0
+left, right = 0, coordinate[-1]
 while left <= right:
     mid = (left + right) // 2
     count = install_wifi(mid)
     if count < C:
+        right = mid - 1
+    else:
+        left = mid + 1
+        answer = mid
+print(answer)
