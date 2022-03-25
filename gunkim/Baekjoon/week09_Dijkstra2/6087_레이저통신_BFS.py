d288ea6959b4ef5eb4184cef1f0adf33e6f36353
unknown <harrison1995@naver.com>
2022-03-25 13:27:23 +0900

    bfs 풀이 생각치 못함

--- gunkim/Baekjoon/week09_Dijkstra2/6087_레이저통신_BFS.py
+++ gunkim/Baekjoon/week09_Dijkstra2/6087_레이저통신_BFS.py
@@ -1,23 +1,46 @@
-def bfs(x, y):
-    queue = deque([(x, y)])
-    visited[x][y] = 0
+# 다익스트라로 풀이 시도를 했는데 70%로 에서 막힌다. 이유를 모르겠다.
+# 임의로 bfs 풀이를 보고 풀었다.
+from collections import deque
+import sys
+
+
+W, H = map(int, sys.stdin.readline().split())
+board, location = [], []
+for i in range(H):
+    board.append(list(sys.stdin.readline())[:-1])
+for i in range(H):
+    for j in range(W):
+        if board[i][j] == 'C':
+            location.append([i, j])
+
+visited = [[float("inf")] * W for _ in range(H)]
+go = [[0, 1], [0, -1], [1, 0], [-1, 0]] # 동서남북
+
+
+def bfs(y, x):
+    queue = deque([(y, x)])
+    visited[y][x] = 0
     while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            ## 동 남 서 북 순서
-            nx, ny = x + dx[i], y + dy[i]
-            while True:
-                ## 범위를 벗어난다
-                if not (0 <= nx < n and 0 <= ny < m): break
+        cur_y, cur_x = queue.popleft()
+        for go_y, go_x in go: # 동서남북 이동
+            ny, nx = cur_y + go_y, cur_x + go_x
+            while True: # 한 방향으로 쭉 탐색해본다
+                # 1. 범위가 벗어날 때
+                # 2. 벽을 만날 때
+                # 3. 방문을 이미 한곳인데 더 적은 거울을 이미 사용할 수 있는 곳이라면
+                # 바로 break 하고 다른 방향 탐색을 진행
+                if not (0 <= ny < H and 0 <= nx < W):
+                    break
+                if board[ny][nx] == "*":
+                    break
+                if visited[ny][nx] < visited[cur_y][cur_x] + 1:
+                    break
+                queue.append((ny, nx)) # 유효하게 이동한 곳이면 queue에 모두 추가
+                visited[ny][nx] = visited[cur_y][cur_x] + 1 # cur_y, cur_x 좌표의 visited 값에서 1을 더하는 것 유의
+                # 같은 방향 한 칸 이동 갱신
+                ny += go_y
+                nx += go_x
 
-                ## 벽을 만난다
-                if board[nx][ny] == '*': break
 
-                ## 지난 적 있는 곳인데, 지금 경로로는 너무 많은 거울이 필요해서 break
-                if visited[nx][ny] < visited[x][y] + 1: break
-
-                ## board업데이트, queue 추가
-                queue.append((nx, ny))
-                visited[nx][ny] = visited[x][y] + 1
-                nx = nx + dx[i]
-                ny = ny + dy[i]
+bfs(location[0][0], location[0][1])
+print(visited[location[1][0]][location[1][1]] - 1) # 첫 출발은 거울 없이 모든 방향 이동 가능하기에 1을 빼도 된다
