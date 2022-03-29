8dad6f2c02493e2d0b5b89b1a1461d4831a24773
kimgun95 <harrison1995@naver.com>
2022-03-29 16:59:02 +0900

    cant solve

---
+++ gunkim/Baekjoon/week10_TwoPointer/2118_두개의탑.py
@@ -0,0 +1,14 @@
+import sys
+
+n = int(sys.stdin.readline())
+distance = []
+total = 0
+for _ in range(n):
+    x = int(sys.stdin.readline())
+    distance.append(x)
+    total += x
+
+# for i in range(n):
+#     for j in range
+print(distance)
+print(distance[2:-1])
