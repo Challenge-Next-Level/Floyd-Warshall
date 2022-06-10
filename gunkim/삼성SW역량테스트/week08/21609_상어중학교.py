ec27a98e1a376e81a2e5a2a237977f27b72db0fa
FBI00851 <rlarjs7879@pionnet.co.kr>
2022-06-10 17:04:43 +0900

    test commit

---
+++ gunkim/삼성SW역량테스트/week08/21609_상어중학교.py
@@ -0,0 +1,22 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+board = []
+for _ in range(n):
+    board.append(list(map(int, sys.stdin.readline().split())))
+
+
+
+# 블록 그룹: 일반 블록 하나 이상 및 같은 색상, 검은 블록 x, 무지개 블록 상관 x
+
+
+# 1. 가장 큰 블록 찾기
+
+# 2. 블록 그룹 제거, 점수 획득
+
+# 3. 중력 작용
+
+# 4. 반시계 방향 회전
+
+# 5. 중력 작용2
+
