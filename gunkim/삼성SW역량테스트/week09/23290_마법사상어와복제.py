9818dbb9af395dc431ab20559aafc91d5b325ac6
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2022-06-15 23:14:38 +0900

    입력값을 생각해보고 단순하게 생각하자:내풀이 x

--- gunkim/삼성SW역량테스트/week09/23290_마법사상어와복제.py
+++ gunkim/삼성SW역량테스트/week09/23290_마법사상어와복제.py
@@ -1,41 +1,97 @@
-import sys
+# 내가 푼 풀이 x
+# 내 풀이로 풀면서 느꼈던 문제는 굳이 물고기의 좌표를 따로 관리하지 않는게 나았던 것
+# board의 크기도 4*4라 이중 for문을 반복하는데 문제가 없었을 것(아래 풀이처럼)
 import copy
 
-m, s = map(int, sys.stdin.readline().split())
-board = [[[] for _ in range(4)] for _ in range(4)] # 격자판
-fish = []
-for _ in range(m):
-    fy, fx, d = map(int, sys.stdin.readline().split())
-    fx -= 1
-    fy -= 1
-    d -= 1
-    board[fy][fx].append([d, 1])
-    fish.append([fy, fx, d])
-sy, sx = map(int, sys.stdin.readline().split())
-sy -= 1
-sx -= 1
-board[sy][sx].append([-1, 2])
-shark = [sy, sx]
+def move_fish():
+    """
+    물고기 이동
+    1. 상어가 있는 칸, 물고기 냄새 칸, 벗어나는 칸 x
+    2. 45도 반시계 회전 후 이동. 이동 못하는 경우 그대로
+    :return:
+    """
+    res = [[[] for _ in range(4)] for _ in range(4)]
+    for x in range(4):
+        for y in range(4):
+            while temp[x][y]:
+                d = temp[x][y].pop()
+                for i in range(d, d - 8, -1):
+                    i %= 8
+                    nx, ny = x + f_dx[i], y + f_dy[i]
+                    if (nx, ny) != shark and 0 <= nx < 4 and 0 <= ny < 4 and not smell[nx][ny]:
+                        res[nx][ny].append(i)
+                        break
+                else:
+                    res[x][y].append(d)
+    return res
 
-dy = [0, -1, -1, -1, 0, 1, 1, 1]
-dx = [-1, -1, 0, 1, 1, 1, 0, -1]
+def dfs(x, y, dep, cnt, visit):
+    """
+    상어 3칸 이동
+    1. 제외되는 물고기 수가 많고 > 이동방법 사전순(백트래킹하면 자동으로 됨)
+    2. 이동한 곳을 저장 > 물고기 냄새가 됨
+    """
+    global max_eat, shark, eat
+    if dep == 3:   # 3번 이동한 경우 그만
+        if max_eat < cnt:
+            max_eat = cnt
+            shark = (x, y)
+            eat = visit[:]
+        return
+    for d in range(4):
+        nx = x + dx[d]
+        ny = y + dy[d]
+        if 0 <= nx < 4 and 0 <= ny < 4:
+            if (nx, ny) not in visit:  # 처음 방문, cnt에 죽은 물고기 수 추가
+                visit.append((nx, ny))
+                dfs(nx, ny, dep + 1, cnt + len(temp[nx][ny]), visit)
+                visit.pop()
+            else:  # 방문한 경우
+                dfs(nx, ny, dep + 1, cnt, visit)
 
-for i in range(s):
-# 1. 모든 물고기에게 복제 마법
-    fish_copy = copy.deepcopy(fish)
-# 2. 모든 물고기 한 칸 이동
-    for f in range(len(fish)):
-        y, x, di = fish[f]
-        flag = 0
-        for j in range(8):
-            ny, nx = y + dy[(di + j) % 8], x + dx[(di + j) % 8]
-            if 0 <= ny < 4 and 0 <= nx < 4 and (len(board[ny][nx]) == 0 or board[ny][nx][0][1] != 3):
-                flag = 1
-                fish[f] = [ny, nx, (di + j) % 8]
-                board
+#       ←, ↖,   ↑,  ↗, →, ↘, ↓, ↙
+f_dx = [0, -1, -1, -1, 0, 1, 1, 1]
+f_dy = [-1, -1, 0, 1, 1, 1, 0, -1]
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
 
-# 3. 상어가 3칸 이동, 물고기가 있다면 죽음 및 냄새를 남김, 물고기 수 > 사전순
+m, s = map(int, input().split())
+fish = [list(map(int, input().split())) for _ in range(m)]
+graph = [[[] for _ in range(4)] for _ in range(4)]
 
-# 4. 2초가 지난 곳 냄새는 사라짐
+for x, y, d in fish:
+    graph[x - 1][y - 1].append(d - 1)
 
-# 5. 복제 마법 적용
+shark = tuple(map(lambda x: int(x) - 1, input().split()))
+smell = [[0] * 4 for _ in range(4)]
+
+for _ in range(s):
+    eat = list()
+    max_eat = -1
+    # 1. 모든 물고기 복제
+    temp = copy.deepcopy(graph)
+    # 2. 물고기 이동
+    temp = move_fish()
+    # 3. 상어이동 - 백트래킹
+    dfs(shark[0], shark[1],0, 0, list())
+    for x, y in eat:
+        if temp[x][y]:
+            temp[x][y] = []
+            smell[x][y] = 3   # 3번 돌아야 없어짐
+    # 4. 냄새 사라짐
+    for i in range(4):
+        for j in range(4):
+            if smell[i][j]:
+                smell[i][j] -= 1
+    # 5. 복제 마법
+    for i in range(4):
+        for j in range(4):
+            graph[i][j] += temp[i][j]
+
+# 물고기 수 구하기
+answer = 0
+for i in range(4):
+    for j in range(4):
+        answer += len(graph[i][j])
+
+print(answer)
