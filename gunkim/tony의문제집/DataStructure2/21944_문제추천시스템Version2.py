e6efc0589651854c11507eb4a1dba60b657c2648
FBI00851 <rlarjs7879@pionnet.co.kr>
2022-06-30 11:04:26 +0900

    틀린이유가 뭘까

--- gunkim/tony의문제집/DataStructure2/21944_문제추천시스템Version2.py
+++ gunkim/tony의문제집/DataStructure2/21944_문제추천시스템Version2.py
@@ -7,45 +7,47 @@ idx = 0
 visited = [False for _ in range(n)]
 minHeap = []
 maxHeap = []
-divisionMinHeap = {}
-divisionMaxHeap = {}
+divisionMinHeap = [[] for _ in range(101)]
+divisionMaxHeap = [[] for _ in range(101)]
+difficultyMinHeap = [[] for _ in range(101)]
+difficultyMaxHeap = [[] for _ in range(101)]
 dict = {}
 for _ in range(n):
     P, L, G = map(int, sys.stdin.readline().split()) # 문제번호, 난이도, 알고리즘분류
     heapq.heappush(minHeap, [L, P, idx])
     heapq.heappush(maxHeap, [-L, -P, idx])
-    if G not in divisionMinHeap:
-        divisionMinHeap[G] = []
-        divisionMaxHeap[G] = []
     heapq.heappush(divisionMinHeap[G], [L, P, idx])
     heapq.heappush(divisionMaxHeap[G], [-L, -P, idx])
+    heapq.heappush(difficultyMinHeap[L], [P, idx])
+    heapq.heappush(difficultyMaxHeap[L], [-P, idx])
     dict[P] = idx
     idx += 1
 m = int(input())
 for _ in range(m):
     case = list(sys.stdin.readline().split())
-    if case[0] == 'add':
-        heapq.heappush(minHeap, [int(case[2]), int(case[1]), idx])
-        heapq.heappush(maxHeap, [-int(case[2]), -int(case[1]), idx])
-        if int(case[3]) not in divisionMinHeap:
-            divisionMinHeap[int(case[3])] = []
-            divisionMaxHeap[int(case[3])] = []
-        heapq.heappush(divisionMinHeap[int(case[3])], [int(case[2]), int(case[1]), idx])
-        heapq.heappush(divisionMaxHeap[int(case[3])], [-int(case[2]), -int(case[1]), idx])
-        dict[int(case[1])] = idx
+    if case[0] == 'add': # add P L G
+        P, L, G = int(case[1]), int(case[2]), int(case[3])
+        heapq.heappush(minHeap, [L, P, idx])
+        heapq.heappush(maxHeap, [-L, -P, idx])
+        heapq.heappush(divisionMinHeap[G], [L, P, idx])
+        heapq.heappush(divisionMaxHeap[G], [-L, -P, idx])
+        heapq.heappush(difficultyMinHeap[L], [P, idx])
+        heapq.heappush(difficultyMaxHeap[L], [-P, idx])
+        dict[P] = idx
         visited.append(False)
         idx += 1
-    elif case[0] == 'solved':
+    elif case[0] == 'solved': # solved P
         visited[dict[int(case[1])]] = True
     elif case[0] == 'recommend': # recommend G x
-        if case[2] == '1' and divisionMaxHeap[int(case[1])]:
-            while divisionMaxHeap[int(case[1])] and visited[divisionMaxHeap[int(case[1])][0][2]]:
-                heapq.heappop(divisionMaxHeap[int(case[1])])
-            print(-divisionMaxHeap[int(case[1])][0][1])
-        elif case[2] == '-1' and divisionMinHeap[int(case[1])]:
-            while divisionMinHeap[int(case[1])] and visited[divisionMinHeap[int(case[1])][0][2]]:
-                heapq.heappop(divisionMinHeap[int(case[1])])
-            print(divisionMinHeap[int(case[1])][0][1])
+        G = int(case[1])
+        if case[2] == '1' and divisionMaxHeap[G]:
+            while divisionMaxHeap[G] and visited[divisionMaxHeap[G][0][2]]:
+                heapq.heappop(divisionMaxHeap[G])
+            print(-divisionMaxHeap[G][0][1])
+        elif case[2] == '-1' and divisionMinHeap[G]:
+            while divisionMinHeap[G] and visited[divisionMinHeap[G][0][2]]:
+                heapq.heappop(divisionMinHeap[G])
+            print(divisionMinHeap[G][0][1])
     elif case[0] == 'recommend2': # recommend2 x
         if case[1] == '1' and maxHeap:
             while maxHeap and visited[maxHeap[0][2]]:
@@ -56,26 +58,28 @@ for _ in range(m):
                 heapq.heappop(minHeap)
             print(minHeap[0][1])
     elif case[0] == 'recommend3': # recommend3 x L
-        arrayList = []
+        index = int(case[2]) # 난이도
         if case[1] == '1':
-            while minHeap and visited[minHeap[0][2]]:
-                heapq.heappop(minHeap)
-            while minHeap and minHeap[0][0] < int(case[2]):
-                arrayList.append(heapq.heappop(minHeap))
-            if minHeap:
-                print(minHeap[0][1])
-            else:
+            while index < 101:
+                while difficultyMinHeap[index] and visited[difficultyMinHeap[index][0][1]]:
+                    heapq.heappop(difficultyMinHeap[index])
+                if len(difficultyMinHeap[index]) == 0:
+                    index += 1
+                else:
+                    break
+            if index > 100:
                 print(-1)
-            while arrayList:
-                heapq.heappush(minHeap, arrayList.pop())
+            else:
+                print(difficultyMinHeap[index][0][0])
         elif case[1] == '-1':
-            while maxHeap and visited[maxHeap[0][2]]:
-                heapq.heappop(maxHeap)
-            while maxHeap and -maxHeap[0][0] > int(case[2]):
-                arrayList.append(heapq.heappop(maxHeap))
-            if maxHeap:
-                print(-maxHeap[0][1])
-            else:
+            while index > 0:
+                while difficultyMaxHeap[index] and visited[difficultyMaxHeap[index][0][1]]:
+                    heapq.heappop(difficultyMaxHeap[index])
+                if len(difficultyMaxHeap[index]) == 0:
+                    index -= 1
+                else:
+                    break
+            if index < 1:
                 print(-1)
-            while arrayList:
-                heapq.heappush(maxHeap, arrayList.pop())
+            else:
+                print(-difficultyMaxHeap[index][0][0])
