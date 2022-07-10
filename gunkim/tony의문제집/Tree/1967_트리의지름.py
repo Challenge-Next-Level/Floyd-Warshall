eac10d4e0e5220cb9947cf702ace7968856076d1
kimgun95 <harrison1995@naver.com>
2022-07-10 19:06:36 +0900

    재귀활용 트리 길이 구하기

--- gunkim/tony의문제집/Tree/1967_트리의지름.py
+++ gunkim/tony의문제집/Tree/1967_트리의지름.py
@@ -1,4 +1,5 @@
 import sys
+sys.setrecursionlimit(10000)
 
 n = int(input())
 tree = [[] for _ in range(n+1)]
@@ -13,8 +14,18 @@ save = [[] for _ in range(n+1)]
 
 def findAndSave(node):
     if len(tree[node]) == 0:
-        return myWeight[node]
-    elif len(tree[node]) == 1:
-        return max(save[tree[node][0]]) + myWeight[tree[node][0]]
+        return [0]
     else:
-        return max(max(save[tree[node][0]]) + myWeight[tree[node][0]], max(save[tree[node][1]]) + myWeight[tree[node][1]])
+        for i in range(len(tree[node])):
+            save[node].append(max(findAndSave(tree[node][i][0])) + tree[node][i][1])
+    return save[node]
+
+
+findAndSave(1)
+answer = 0
+for i in range(n+1):
+    if len(save[i]) > 0:
+        save[i].sort(reverse=True)
+        answer = max(answer, sum(save[i][:2]))
+
+print(answer)
