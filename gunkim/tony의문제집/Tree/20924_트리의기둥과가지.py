0c65bf1ede8ecefe647e0bcde596beee7a5bcb86
FBI00851 <rlarjs7879@pionnet.co.kr>
2022-07-12 17:43:37 +0900

    시간초과

---
+++ gunkim/tony의문제집/Tree/20924_트리의기둥과가지.py
@@ -0,0 +1,54 @@
+# 2:기둥, 1:기가노드, 0:가지
+import sys
+
+sys.setrecursionlimit(10000)
+n, r = map(int, sys.stdin.readline().split())
+tree = [[] for _ in range(n + 1)]
+for _ in range(n-1):
+    start, end, length = map(int, sys.stdin.readline().split())
+    tree[start].append([end, length])
+    tree[end].append([start, length])
+
+nodeNumber = [0 for _ in range(n+1)]
+
+
+def find(node):
+    global r
+    if len(tree[node]) > 2:
+        nodeNumber[node] = 1
+        return node
+    for num, dist in tree[node]:
+        if nodeNumber[num] == 0:
+            if node == r:
+                nodeNumber[node] = 2
+            nodeNumber[num] = 2
+            return find(num)
+
+
+def dfs(start, isBranch, totalLen): # isBranch: 0 - 가지, 2 - 기둥
+    if len(tree[start]) == 1 and totalLen != 0:
+        global answer1, answer2
+        if isBranch == 2:
+            answer1 = max(answer1, totalLen)
+        else:
+            answer2 = max(answer2, totalLen)
+        return
+    for number, distance in tree[start]:
+        if visit[number] == 0 and nodeNumber[number] == isBranch:
+            visit[number] = 1
+            dfs(number, isBranch, totalLen + distance)
+    return
+
+
+gigaNode = find(r) # 기가 노드를 찾는다
+visit = [0 for _ in range(n+1)]
+answer1, answer2 = 0, 0
+
+if gigaNode is not None:
+    visit[gigaNode] = 1
+    dfs(gigaNode, 2, 0)
+    dfs(gigaNode, 0, 0)
+else:
+    visit[r] = 1
+    dfs(r, 2, 0)
+print(answer1, answer2)
