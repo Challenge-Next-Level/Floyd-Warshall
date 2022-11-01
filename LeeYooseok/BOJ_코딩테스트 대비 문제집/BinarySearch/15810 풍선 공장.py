69787419cb7cbc615b1a3a567f757cd1d3c5bc69
yuseogi0218 <yuseogi0218@gmail.com>
2022-11-01 19:54:19 +0900

    15810

--- LeeYooseok/BOJ_코딩테스트 대비 문제집/BinarySearch/15810 풍선 공장.py
+++ LeeYooseok/BOJ_코딩테스트 대비 문제집/BinarySearch/15810 풍선 공장.py
@@ -1,9 +1,9 @@
 N, M = map(int, input().split())
 staff_list = list(map(int, input().split()))
 
-t_max = max(staff_list)
-start, end = 0, t_max * M
-answer = t_max * M
+t_min = min(staff_list)
+start, end = 0, t_min * M
+answer = t_min * M
 while start <= end:
     mid = (start + end) // 2
 
