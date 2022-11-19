90a1e1b85db517904890a035c2eaf8e7a4f347fc
yuseogi0218 <yuseogi0218@gmail.com>
2022-11-19 20:26:32 +0900

    divide and conquer

--- LeeYooseok/BOJ_코딩테스트 대비 문제집/BackTracking/1174 줄어드는 수.py
+++ LeeYooseok/BOJ_코딩테스트 대비 문제집/BackTracking/1174 줄어드는 수.py
@@ -3,15 +3,17 @@ N = int(input())
 num_array = list()
 decreasing_num_set = set()
 
+
 def dfs():
     if len(num_array) != 0:
         decreasing_num_set.add(int("".join(map(str, num_array))))
 
     for i in range(0, 10):
-        if len(num_array) == 0 or num_array[-1] > i: # 마지막 값이 더 큰 경우
+        if len(num_array) == 0 or num_array[-1] > i:  # 마지막 값이 더 큰 경우
             num_array.append(i)
             dfs()
-            num_array.pop(i)
+            num_array.pop()
+
 
 try:
     dfs()
