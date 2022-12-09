7cb7e48b73cfcc7802e665abc90df4426e464a8a
kimgun95 <54833128+kimgun95@users.noreply.github.com>
2022-12-09 15:13:09 +0900

    조금 어려웠던 그룹화

---
+++ gunkim/SQL/GROUP_BY/lv4_년월성별별상품구매회원수구하기.sql
@@ -0,0 +1,5 @@
+SELECT YEAR(OS.sales_date) Y, MONTH(OS.sales_date) M, UI.gender GENDER, COUNT(DISTINCT UI.user_id)
+FROM USER_INFO UI, ONLINE_SALE OS
+WHERE UI.user_id = OS.user_id AND GENDER is not null
+GROUP BY Y,M,GENDER
+ORDER BY Y,M,GENDER
