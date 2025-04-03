2227c0d2595f6cf3430492dd31864f5c7b350834
yuseogi0218 <yuseogi0218@gmail.com>
2025-04-03 14:55:09 +0900

    2025-04-03 BOJ_오늘의 문제

---
+++ LeeYooseok/BOJ_오늘의 문제/2025/2025-04/G5 - 3067.py
@@ -0,0 +1,16 @@
+T = int(input())
+
+for _ in range(T):
+    N = int(input())
+    coin_list = [0] + list(map(int, input().split()))
+    M = int(input())
+    DP = [0 for _ in range(M + 1)]
+
+    for coin in coin_list:
+        if coin > M:
+            continue
+        DP[coin] += 1  # coin 방법 수 + 1
+        for i in range(coin + 1, M + 1):  # coin + 다른 동전으로 만드는 경우의 수
+            DP[i] += DP[i - coin]
+
+    print(DP[M])
